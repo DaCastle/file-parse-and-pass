a00802980ff6b76f7ec1c416ef0111cefd9d9e70
DaCastle <dakota.castleberg18@gmail.com>
2021-06-25 08:43:55 -0500

    try/cactch added for fileReader to prevent possible crashing

--- src/utilities.js
+++ src/utilities.js
@@ -10,21 +10,25 @@ import XLSX from 'xlsx'
  */
 export const parseSpreadsheetData = async (file) => {
     return new Promise((resolve, reject) => {
-        let reader = new FileReader();
+        try {
+            let reader = new FileReader();
 
-        reader.onload = (e) => {
+            reader.onload = (e) => {
 
-            const data = e.target.result;
-            const convertedData = XLSX.read(data, { type: 'binary' });
-            const worksheetName = convertedData.SheetNames[0];
-            const worksheet = convertedData.Sheets[worksheetName];
-            const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-            resolve(parsedData);
-        };
+                const data = e.target.result;
+                const convertedData = XLSX.read(data, { type: 'binary' });
+                const worksheetName = convertedData.SheetNames[0];
+                const worksheet = convertedData.Sheets[worksheetName];
+                const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+                resolve(parsedData);
+            };
 
-        reader.onerror = reject;
+            reader.onerror = reject;
 
-        reader.readAsBinaryString(file);
+            reader.readAsBinaryString(file);
+        } catch {
+            console.log('file explorer closed before selecting file')
+        }
     })
 }
 
