52028168719d92c448669e065161d631c879ced4
DaCastle <dakota.castleberg18@gmail.com>
2021-06-25 10:54:16 -0500

    test update (and deferred)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Upload from './components/Upload';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * Testing based on/around file upload, especially in this
+ * implementation where:
+ * 1) most of the app does not exist until a file has been uploaded
+ * 2) component state changes occur upon file upload
+ * appear to be trickier then I would like, so only testing
+ * initial render for now.
+ */
+test('renders the upload button', () => {
+  render(<Upload buttonText='Click or Drag excel/csv file here' />);
+  const button = screen.getByText('Click or Drag excel/csv file here');
+  expect(button).toBeInTheDocument();
 });
